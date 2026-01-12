55c3f496e54b4431311a6be7a2a28ccf91d33f42
Daniel Cerutti <daniel@ceruttisoftware.ch>
2026-01-12 11:34:44 +0100

    Start upgrading to xcode 26. Suppress old access through the unsupported DVTFoundation

--- iSimulatorExplorer/DCSimulatorManager.swift
+++ iSimulatorExplorer/DCSimulatorManager.swift
@@ -11,107 +11,12 @@ import Cocoa
 
 class DCSimulatorManager {
     private var simulatorFramework : Bundle?
-    private let kDVTFoundationRelativePath = "../SharedFrameworks/DVTFoundation.framework"
-    private let kDevToolsFoundationRelativePath = "../OtherFrameworks/DevToolsFoundation.framework"
     private var developerDir : String?
     private var simServiceContextClass : AnyClass?
     private var simDeviceSetClass : AnyClass?
     private var simDeviceSet : AnyObject?
     private var getSimulators : () -> [Simulator]
 
-    // Helper to find a class by name and die if it isn't found.
-    private func FindClassByName(_ nameOfClass : String) -> AnyClass! {
-        if let theClass : AnyClass = NSClassFromString(nameOfClass) {
-            return theClass
-        }
-        else
-        {
-            NSLog("Failed to find class %@ at runtime.", nameOfClass)
-            return nil
-        }
-    }
-
-    private func LoadSimulatorFramework() -> Bundle! {
-        if let devDir = XCodeSupport.getDeveloperToolsPath() as NSString? {
-            self.developerDir = devDir as String
-
-            // The Simulator framework depends on some of the other Xcode private
-            // frameworks; manually load them first so everything can be linked up.
-            let dvtFoundationPath = devDir.appendingPathComponent(kDVTFoundationRelativePath)
-            
-            if let dvtFoundationBundle = Bundle(path: dvtFoundationPath) {
-                if !(dvtFoundationBundle.load()) {
-                    return nil
-                }
-            }
-            
-            let devToolsFoundationPath = devDir.appendingPathComponent(kDevToolsFoundationRelativePath)
-            if let devToolsFoundationBundle = Bundle(path: devToolsFoundationPath) {
-                if !(devToolsFoundationBundle.load()) {
-                    return nil
-                }
-            }
-            
-
-            // Prime DVTPlatform.
-            do {
-                try CoreSimulatorHelper.loadAllPlatformsReturningError()
-            }
-            catch let error as NSError {
-                NSLog("Unable to loadAllPlatformsReturningError. Error: %@", error.localizedDescription)
-                return nil;
-            }
-            catch {
-                NSLog("Unable to loadAllPlatformsReturningError. Unknown Error")
-                return nil;
-            }
-
-            // The path within the developer dir of the private Simulator frameworks.
-            let simulatorFrameworkRelativePath = "../SharedFrameworks/DVTiPhoneSimulatorRemoteClient.framework";
-            let kCoreSimulatorRelativePath = "Library/PrivateFrameworks/CoreSimulator.framework";
-            let coreSimulatorPath = devDir.appendingPathComponent(kCoreSimulatorRelativePath);
-            if let coreSimulatorBundle = Bundle(path: coreSimulatorPath) {
-                if !(coreSimulatorBundle.load()) {
-                    return nil
-                }
-            }
-            let simBundlePath = devDir.appendingPathComponent(simulatorFrameworkRelativePath);
-            if let simBundle = Bundle(path: simBundlePath) {
-                if !(simBundle.load()) {
-                    return nil
-                }
-                return simBundle;
-            }
-        }
-        return nil;
-    }
-
-    
-    // Get XCode version 8 simulators using CoreSimulator framework
-    fileprivate func getXcode8SimulatorsFromCoreSimulator() -> [Simulator] {
-
-        var simulators = [Simulator]()
-        
-        //if let context = try? simServiceContextClass!.sharedServiceContext(forDeveloperDir: developerDir!) {
-        if let context = try? SimServiceContextWrapper.sharedServiceContext(forDeveloperDir: developerDir!, simServiceContextClass:simServiceContextClass!) {
-
-            simDeviceSet = try! context.defaultDeviceSet()
-            
-            if simDeviceSet != nil  {
-                if let deviceList = simDeviceSet!.availableDevices {
-                    for simDevice in deviceList! {
-                        let sim = Simulator(device: simDevice as AnyObject)
-                        if sim.isValid {
-                            simulators.append(sim)
-                        }
-                    }
-                }
-            }
-
-        }
- 
-        return simulators;
-    }
     
     // Get XCode simulators by browsing the file system
     private func getXcode8SimulatorsFromFileSystem() -> [Simulator] {
@@ -152,25 +57,15 @@ class DCSimulatorManager {
         if let dtVersion = XCodeSupport.getDeveloperToolsVersion() {
             
             NSLog("XCode version \(dtVersion)")
-            if dtVersion.compare("8.0", options: NSString.CompareOptions.numeric) == ComparisonResult.orderedAscending {
+            if dtVersion.compare("15.0", options: NSString.CompareOptions.numeric) == ComparisonResult.orderedAscending {
                 
-                NSLog("XCode version < 8.0. Not Supported")
+                NSLog("XCode version < 15.0. Not Supported")
                 
             }
             else {
-                NSLog("XCode version >= 8.0")
+                NSLog("XCode version >= 15.0")
 
-                // XCode version 8.0 simulators
-                simulatorFramework = LoadSimulatorFramework()
-                simDeviceSetClass = FindClassByName("SimDeviceSet")
-                simServiceContextClass = FindClassByName("SimServiceContext")
-                if simServiceContextClass != nil {
-                    getSimulators = getXcode8SimulatorsFromCoreSimulator
-                }
-                else
-                {
-                    getSimulators = getXcode8SimulatorsFromFileSystem
-                }
+                getSimulators = getXcode8SimulatorsFromFileSystem
             }
         }
     }
